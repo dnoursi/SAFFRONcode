c6858ac04784b6e30ec835e527944c29e5efca8e
David Noursi <dnoursi@uchicago.edu>
2019-03-19 19:18:23 -0500

    misc

--- exp_FDR_batch_new.py
+++ exp_FDR_batch_new.py
@@ -19,16 +19,16 @@ from SAFFRON_ALPHA_INV import*
 from rowexp_new_batch import*
 from toimport import*
 from settings_util import*
-  
+
 ################ Running entire framework  ####################
 
 def run_single(NUMRUN, NUMHYP, NUMDRAWS, mu_gap, pi, alpha0, markov_lag, mod_choice, FDR, sigma = 1, verbose = False, TimeString = False, rndseed = 0, startfac = 0.1):
 # model choice 1: Gaussian mixture; 2: Beta alternatives
-    
+
     if rndseed == 0:
         TimeString = True
 
-    ##------------- Setting hypotheses, penalty and prior weights -------------## 
+    ##------------- Setting hypotheses, penalty and prior weights -------------##
     if TimeString:
         time_str = datetime.today().strftime("%m%d%y_%H%M")
     else:
@@ -37,11 +37,11 @@ def run_single(NUMRUN, NUMHYP, NUMDRAWS, mu_gap, pi, alpha0, markov_lag, mod_cho
     Hypo = get_hyp(pi, NUMHYP)
     Hypo = Hypo.astype(int)
     num_alt = np.sum(Hypo)
-       
+
     #### Set file and dirnames ##########
     dir_name = './dat'
     filename = 'MG%.1f_Si%.1f_FDR%d_NH%d_ND%d_L%d_PM%.2f_NR%d_MOD%d_%s' % (mu_gap, sigma, FDR, NUMHYP, NUMDRAWS, markov_lag, pi, NUMRUN, mod_choice, time_str)
-    
+
     # ----------- initialize result vectors and mats ------------- ##
     pval_mat = np.zeros([NUMHYP, NUMRUN])
     rej_mat = np.zeros([NUMHYP, NUMRUN])
@@ -55,7 +55,7 @@ def run_single(NUMRUN, NUMHYP, NUMDRAWS, mu_gap, pi, alpha0, markov_lag, mod_cho
     falrej_mat = np.zeros([NUMHYP, NUMRUN])
     correj_mat = np.zeros([NUMHYP, NUMRUN])
 
-    # ----------------- Run experiments (with same mus) ------------------ # 
+    # ----------------- Run experiments (with same mus) ------------------ #
     for l in range(NUMRUN):
 
         #%%%%%%%%  Initialize theta_j, FDR and experiments %%%%%%%%#
@@ -67,7 +67,9 @@ def run_single(NUMRUN, NUMHYP, NUMDRAWS, mu_gap, pi, alpha0, markov_lag, mod_cho
 
         # Create a vector of gaps
         gap = np.ones(NUMHYP)*mu_gap
-        this_exp = rowexp_new_batch(NUMHYP, NUMDRAWS, Hypo, 0, gap, markov_lag)
+        this_exp = bikeexp_new_batch(NUMHYP, NUMDRAWS, Hypo, 0, gap, markov_lag)
+        #defn: rowexp_new_batch(NUMHYP, numdraws, alt_vec, mu0, mu_alt_vec, markov_lag)
+        #this_exp = rowexp_new_batch(NUMHYP, NUMDRAWS, Hypo, 0, gap, markov_lag)
 
         #%%%%%%%%% Run experiments: Get sample and p-values etc. %%%%%%%%%%%%%
         # Run random experiments with same random seed for all FDR procedures
@@ -113,7 +115,7 @@ def run_single(NUMRUN, NUMHYP, NUMDRAWS, mu_gap, pi, alpha0, markov_lag, mod_cho
                 FDR_vec[j] = 0
 
         FDR_mat[:,l] = FDR_vec
-        
+
     # -----------------  Compute average quantities we care about ------------- #
 
 
@@ -126,4 +128,3 @@ def run_single(NUMRUN, NUMHYP, NUMDRAWS, mu_gap, pi, alpha0, markov_lag, mod_cho
     # Save data
     data = np.r_[FDR_mat, rej_mat, falrej_mat, wealth_mat, pval_mat, alpha_mat, np.expand_dims(TDR_vec, axis=0), np.expand_dims(np.asarray(FDR_vec),axis=0)]
     saveres(dir_name, filename, data)
-
